dd13308e0a160e335e23f24815a62b121503a316
Luiz Fernando da Silva Gazoli <fernandogazoli@hotmail.com>
2022-11-25 19:18:02 -0300

    Extrato Pessoal

---
+++ ExerciciosPython/Projeto2Extratopessoal.py
@@ -0,0 +1,188 @@
+'''
+Auto: Luiz Fernando da Silva Gazoli
+Data início: 19/11/2022
+Projeto: Personal Bank Statement
+
+
+
+
+'''
+
+nome = str(input('Digite seu nome: '))
+print(nome)
+banco = str(input('Digite seu banco: '))
+print(banco)
+taxa_mes_vigente = float(input('Digite a taxa do mês: '))
+print('Taxa do Mês =  %')
+
+entrada_dia1 = float(input('Digite Depósito R$ '))
+saida = float(input('Digite retirada R$ '))
+resultado1 = entrada_dia1 - saida
+
+entrada_dia2 = float(input('Digite Depósito R$ '))
+saldo2 = resultado1 + entrada_dia2
+saida2 = float(input('Digite retirada R$ '))
+resultado2 = saldo2 - saida2
+
+entrada_dia3 = float(input('Digite Depósito R$ '))
+saldo3 = resultado2 + entrada_dia3
+saida3 = float(input('Digite retirada R$ '))
+resultado3 = saldo3 - saida3
+
+entrada_dia4 = float(input('Digite Depósito R$ '))
+saldo4 = resultado3 + entrada_dia4
+saida4 = float(input('Digite retirada R$ '))
+resultado4 = saldo4 - saida4
+
+entrada_dia5 = float(input('Digite Depósito R$ '))
+saldo5 = resultado4 + entrada_dia5
+saida5 = float(input('Digite retirada R$ '))
+resultado5 = saldo5 - saida5
+
+entrada_dia6 = float(input('Digite Depósito R$ '))
+saldo6 = resultado5 + entrada_dia6
+saida6 = float(input('Digite retirada R$ '))
+resultado6 = saldo6 - saida6
+
+entrada_dia7 = float(input('Digite Depósito R$ '))
+saldo7 = resultado6 + entrada_dia7
+saida7 = float(input('Digite retirada R$ '))
+resultado7 = saldo7 - saida7
+
+
+entrada_dia8 = float(input('Digite Depósito R$ '))
+saldo8 = resultado7 + entrada_dia8
+saida8 = float(input('Digite retirada R$ '))
+resultado8 = saldo8 - saida8
+
+entrada_dia9 = float(input('Digite Depósito R$ '))
+saldo9 = resultado8 + entrada_dia9
+saida9 = float(input('Digite retirada R$ '))
+resultado9 = saldo9 - saida9
+
+entrada_dia10 = float(input('Digite Depósito R$ '))
+saldo10 = resultado9 + entrada_dia10
+saida10 = float(input('Digite retirada R$ '))
+resultado10 = saldo10 - saida10
+
+entrada_dia11 = float(input('Digite Depósito R$ '))
+saldo11 = resultado10 + entrada_dia11
+saida11 = float(input('Digite retirada R$ '))
+resultado11 = saldo11 - saida11
+
+entrada_dia12 = float(input('Digite Depósito R$ '))
+saldo12 = resultado11 + entrada_dia12
+saida12 = float(input('Digite retirada R$ '))
+resultado12 = saldo12 - saida12
+
+entrada_dia13 = float(input('Digite Depósito R$ '))
+saldo13 = resultado12 + entrada_dia13
+saida13 = float(input('Digite retirada R$ '))
+resultado13 = saldo13 - saida13
+
+entrada_dia14 = float(input('Digite Depósito R$ '))
+saldo14 = resultado13 + entrada_dia14
+saida14 = float(input('Digite retirada R$ '))
+resultado14 = saldo14 - saida14
+
+entrada_dia15 = float(input('Digite Depósito R$ '))
+saldo15 = resultado14 + entrada_dia15
+saida15 = float(input('Digite retirada R$ '))
+resultado15 = saldo15 - saida15
+
+entrada_dia16 = float(input('Digite Depósito R$ '))
+saldo16 = resultado15 + entrada_dia16
+saida16 = float(input('Digite retirada R$ '))
+resultado16 = saldo16 - saida16
+
+entrada_dia17 = float(input('Digite Depósito R$ '))
+saldo17 = resultado16 + entrada_dia17
+saida17 = float(input('Digite retirada R$ '))
+resultado17 = saldo17 - saida17
+
+entrada_dia18 = float(input('Digite Depósito R$ '))
+saldo18 = resultado17 + entrada_dia18
+saida18 = float(input('Digite retirada R$ '))
+resultado18 = saldo18 - saida18
+
+entrada_dia19 = float(input('Digite Depósito R$ '))
+saldo19 = resultado18 + entrada_dia19
+saida19 = float(input('Digite retirada R$ '))
+resultado19 = saldo19 - saida19
+
+entrada_dia20 = float(input('Digite Depósito R$ '))
+saldo20 = resultado19 + entrada_dia20
+saida20 = float(input('Digite retirada R$ '))
+resultado20 = saldo20 - saida20
+
+entrada_dia21 = float(input('Digite Depósito R$ '))
+saldo21 = resultado20 + entrada_dia21
+saida21 = float(input('Digite retirada R$ '))
+resultado21 = saldo21 - saida21
+
+entrada_dia22 = float(input('Digite Depósito R$ '))
+saldo22 = resultado21 + entrada_dia22
+saida22 = float(input('Digite retirada R$ '))
+resultado22 = saldo22 - saida22
+
+entrada_dia23 = float(input('Digite Depósito R$ '))
+saldo23 = resultado22 + entrada_dia23
+saida23 = float(input('Digite retirada R$ '))
+resultado23 = saldo23 - saida23
+
+entrada_dia24 = float(input('Digite Depósito R$ '))
+saldo24 = resultado23 + entrada_dia24
+saida24 = float(input('Digite retirada R$ '))
+resultado24 = saldo24 - saida24
+
+entrada_dia25 = float(input('Digite Depósito R$ '))
+saldo25 = resultado24 + entrada_dia25
+saida25 = float(input('Digite retirada R$ '))
+resultado25 = saldo25 - saida25
+
+entrada_dia26 = float(input('Digite Depósito R$ '))
+saldo26 = resultado25 + entrada_dia26
+saida26 = float(input('Digite retirada R$ '))
+resultado26 = saldo26 - saida26
+
+entrada_dia27 = float(input('Digite Depósito R$ '))
+saldo27 = resultado26 + entrada_dia27
+saida27 = float(input('Digite retirada R$ '))
+resultado27 = saldo27 - saida27
+
+entrada_dia28 = float(input('Digite Depósito R$ '))
+saldo28 = resultado27 + entrada_dia28
+saida28 = float(input('Digite retirada R$ '))
+resultado28 = saldo28 - saida28
+
+entrada_dia29 = float(input('Digite Depósito R$ '))
+saldo29 = resultado28 + entrada_dia29
+saida29 = float(input('Digite retirada R$ '))
+resultado29 = saldo29 - saida29
+
+entrada_dia30 = float(input('Digite Depósito R$ '))
+saldo30 = resultado29 + entrada_dia30
+saida30 = float(input('Digite retirada R$ '))
+resultado30 = saldo30 - saida30
+
+entrada_dia31 = float(input('Digite Depósito R$ '))
+saldo31 = resultado30 + entrada_dia31
+saida31 = float(input('Digite retirada R$ '))
+resultado31 = saldo31 - saida31
+
+
+resultado = resultado31
+
+saldo_Total = saldo31 * taxa_mes_vigente
+
+palavra = ' ***Demonstrativo Mensal***'
+
+print(palavra)
+
+while resultado > resultado1:
+    resultado1 += 1
+    print(f'Saldo dia R$ {resultado1}')
+
+print(f'Resultado do mês R$ {saldo_Total}')
+
+
